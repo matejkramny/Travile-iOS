dcef81960a7e729f37c176a199fa8fdafa864dad
Matej Kramny <matejkramny@gmail.com>
2012-05-10 23:19:36 +0100

    Initial Commit

---
+++ Travian Manager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Travian Manager
+//
+//  Created by Matej Kramny on 10/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
