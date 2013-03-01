ba6e75a5279bcd2e96723a96506bd33a2fc99635
Matej Kramny <matejkramny@gmail.com>
2013-03-01 23:45:30 +0000

    Add Unit testing (using GHUnit), add TTTest to test the unit testing.

---
+++ Tests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by Matej Kramny on 01/03/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+	}
+}
